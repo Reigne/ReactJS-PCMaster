13faf88983751ed9ae7c2ff688b1643c3bbc7923
Reigne <monteronaelija@yahoo.com>
2024-05-30 23:13:48 +0800

    change cors

--- backend/app.js
+++ backend/app.js
@@ -7,16 +7,31 @@ const order = require("./routes/order");
 var cors = require("cors");
 const app = express();
 // const fileUpload = require('express-fileupload')
-app.use(cors({
-    origin: "https://pc-master-api.vercel.app",
-    credentials: true
-}));
+// app.use(cors({
+//     origin: "https://pc-master-api.vercel.app",
+//     credentials: true
+// }));
+
+const allowedOrigins = ["https://pc-master-api.vercel.app"];
+
 // app.use(express.json());
 // app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cookieParser());
 app.use(express.json({ limit: "100mb" }));
 app.use(express.urlencoded({ limit: "100mb", extended: true }));
 
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
 // app.use(fileUpload());
 
 app.use("/api/v1", products);
