a19fc59fc58ea7f0de1d2bf5eb7998308986a4c3
Reigne <monteronaelija@yahoo.com>
2024-05-30 22:35:11 +0800

    fix bug in deployment

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,33 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
 import { Provider } from 'react-redux';
 import store from './store';
 import App from './App';
 
-// import { positions, transitions, Provider as AlertProvider } from 'react-alert'
-// import AlertTemplate from 'react-alert-template-basic'
-
 import { ToastContainer } from 'react-toastify';
 
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
-// const options = {
-//   timeout: 5000,
-//   position: positions.BOTTOM_CENTER,
-//   transition: transitions.SCALE
-// }
-
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <Provider store={store}>
     <BrowserRouter>
-      {/* <AlertProvider template={AlertTemplate} {...options}> */}
       <App />
       <ToastContainer />
-      {/* </AlertProvider> */}
     </BrowserRouter>
-  </Provider>,
-  document.getElementById('root')
+  </Provider>
 );
